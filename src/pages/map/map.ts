987aea1a2fbed468bf84c8825309fa53547218dd
Ken Sodemann <ken@ionic.io>
2017-12-18 13:33:16 -0600

    feat(map): add a simple map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google: any;
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
 })
 export class MapPage {
+  private map;
+  private marker;
+
   constructor(private navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    this.createMap();
+  }
+
+  private createMap() {
+    this.map = new google.maps.Map(
+      document.getElementById('iss-tracking-map'),
+      {
+        center: new google.maps.LatLng(43.074237, -89.381012),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+    );
+    this.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(43.074237, -89.381012),
+      map: this.map,
+      title: 'Ionic HQ',
+      animation: google.maps.Animation.DROP
+    });
+  }
 }
