b20f4c0c1b9039d53817849fad94e35b3657a3b8
Ken Sodemann <ken@ionic.io>
2018-05-04 17:26:32 -0500

    refactor(v4): add the astronauts page

--- src/app/pages/astronauts/astronauts.page.ts
+++ src/app/pages/astronauts/astronauts.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Astronaut } from '../../models/astronaut';
+import { IssTrackingDataService } from '../../services/iss-tracking-data/iss-tracking-data.service';
 
 @Component({
   selector: 'app-page-contact',
   templateUrl: 'astronauts.page.html',
   styleUrls: ['astronauts.page.scss']
 })
-export class AstronautsPage {}
+export class AstronautsPage {
+  astronauts: Array<Astronaut>;
+
+  constructor(private data: IssTrackingDataService) {}
+
+  ionViewDidEnter() {
+    this.data.astronauts().subscribe(a => (this.astronauts = a));
+  }
+}
